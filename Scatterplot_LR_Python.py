96e071b857859c54415ed8300febbeeedd47797d
madycrucs713 <madycrucs713@gmail.com>
2024-07-02 13:09:08 -0400

    Add files via upload

---
+++ Scatterplot_LR_Python.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[25]:
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+data = pd.read_csv ('regrex1.csv')
+
+x = data [['x']]
+y = data ['y']
+
+plt.figure(figsize=(10, 8))
+plt.scatter(data['x'], data['y'], color='cornflowerblue')
+
+model = LinearRegression()
+model.fit(x, y)
+
+y_pred = model.predict(x)
+
+plt.plot(data['x'], y_pred, color='maroon')
+
+plt.xlabel('X axis')
+plt.ylabel('Y axis')
+plt.title('Scatter Plot with Linear Model Using Python')
+
+plt.show()
+
+
+# In[ ]:
+
+
+
+
